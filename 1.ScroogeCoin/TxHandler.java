4c550fe93cb17366c11b4049520449e5e39ed7af
Uday Varikuti <udaykumar0496@gmail.com>
2021-12-26 15:00:38 -0800

    Updated methods in TxHandler to check the validity of the transaction

---
+++ 1.ScroogeCoin/TxHandler.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class TxHandler {
+
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+
+    private UTXOPool currutxoPool;
+
+    public TxHandler(UTXOPool utxoPool) {
+        this.currutxoPool = new UTXOPool(utxoPool);
+    }
+
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+     * (2) the signatures on each input of {@code tx} are valid, 
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     values; and false otherwise.
+     */
+    public boolean isValidTx(Transaction tx) {
+        ArrayList<Transaction.Input> txInputs = tx.getInputs();
+        ArrayList<Transaction.Output> txOutputs = tx.getOutputs();
+        ArrayList<UTXO> txUTXO = new ArrayList<UTXO>();
+        double inputValue = 0;
+        double outputValue = 0;
+
+        for (int i = 0; i < tx.numInputs(); i++) {
+            Transaction.Input txInput = txInputs.get(i);
+            UTXO utxo = new UTXO(txInput.prevTxHash, txInput.outputIndex);
+            Transaction.Output outputObj = currutxoPool.getTxOutput(utxo);
+            // 1. all outputs claimed are in the current UTXO pool,
+            if (!currutxoPool.contains(utxo))
+                return false;
+            // 2. verify signature
+            byte[] message = tx.getRawDataToSign(txInput.outputIndex);
+            if(!Crypto.verifySignature(outputObj.address, message, txInput.signature))
+                return false;
+            //3. no UTXO is claimed multiple times
+            if (txUTXO.contains(utxo))
+                return false;
+            txUTXO.add(utxo);
+            inputValue += outputObj.value;
+        }
+        for (Transaction.Output output : txOutputs) {
+            // 4. all of the output values are non-negative
+            if (output.value < 0)
+                return false;
+            outputValue += output.value;
+        }
+        //5. the sum of all input values is greater than or equal to the sum of its output values; and false otherwise.
+        if (inputValue < outputValue)
+            return false;
+        return true;
+    }
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        
+    }
+
+}
